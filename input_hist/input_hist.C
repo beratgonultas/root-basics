c0cfa81efc1e6e348f566a3c2d898c9fbf24c228
brt898 <brt_501@hotmail.com>
2021-01-11 22:20:29 +0300

    Added a histogram reading data from a file

---
+++ input_hist/input_hist.C
@@ -0,0 +1,25 @@
+
+{
+	TH1F *hist1 = new TH1F ("hist1", "Grade Distribution", 8, 1, 9);
+
+
+	fstream file;
+	file.open("data_ih.txt", ios::in);
+	
+	double value;
+
+	while(1)
+	{
+		file >> value;
+		hist1->Fill(value);
+		if(file.eof()) break;
+	}
+
+	file.close();
+
+	hist1->GetXaxis()->SetTitle("Grade");
+	hist1->GetYaxis()->SetTitle("Number");
+
+	TCanvas *c1 = new TCanvas ();
+	hist1->Draw();
+}
